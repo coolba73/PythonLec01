9be759a8ba365eda84bbbe7ae9dab3b3d4f57443
coolba73 <coolba73@outlook.com>
2017-10-18 16:53:47 +0900

    1

--- GetStockData.py
+++ GetStockData.py
@@ -1,27 +1,14 @@
-
-
-
-itemCode = "005930"
-
 from pandas_datareader import data
 
+itemCode = "005930"
 df = data.DataReader("KRX:" + itemCode,"google" )
 
-# print(df.head())
-
-# print(df.columns)
-# print(df.index)
-
-# print(df.index.strftime('%Y-%m-%d'))
-
 df.index = df.index.strftime('%Y-%m-%d')
-
 
 df = df.drop('Open',1)
 df = df.drop('High',1)
 df = df.drop('Low',1)
 df = df.drop('Volume',1)
-
 
 print(df.to_json())
 
