a77e0e034920a03c55ead9ce5fc219de1bc44cd8
coolba73 <coolba73@outlook.com>
2017-11-10 22:12:53 +0900

    1

--- GetStockListPrice03.py
+++ GetStockListPrice03.py
@@ -3,7 +3,8 @@ from pandas_datareader import data
 from collections import OrderedDict
 
 items = OrderedDict()
-re = OrderedDict()
+
+re = {}
 
 items["item"] = ["005930","066570"]
 # items["item"] = ["095570","054620"]
@@ -12,16 +13,17 @@ for item in items["item"]:
 
     df = data.DataReader("KRX:" + item,"google" )
     df.index = df.index.strftime('%Y-%m-%d')
-    re[item] = json.dumps(json.loads(df.reset_index().to_json(orient='records')), indent=2)
+    # re[item] = json.dumps(json.loads(df.reset_index().to_json(orient='records')), indent=2)
+    re[item] = json.loads(df.reset_index().to_json(orient='records'))
 
     # print(df.to_json()["Close"])   
 
 
-    print( json.dumps(json.loads(df.reset_index().to_json(orient='records')), indent=2) )
-
-# print(re)
+    # print( json.dumps(json.loads(df.reset_index().to_json(orient='records')), indent=2) )
 
 print(json.dumps(re))
+
+#print( re )
 
 
 
