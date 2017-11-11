099ecb6b4a03975f1859bbcca493649582cd35e2
coolba73 <jaehoon73.choi@outlook.com>
2017-11-11 13:32:32 +0900

    1

--- CalYield.py
+++ CalYield.py
@@ -7,12 +7,28 @@ prices = pandas.DataFrame([1035.23, 1032.47, 1011.78, 1010.59, 1016.03, 1007.95,
 
 # print(prices)
 
+df = pandas.concat([  prices,  prices.shift(-1) , prices / prices.shift(-1) ], axis=1)
+
+print( prices / prices.shift(-1) )
+
 # print(prices[1:] )
 
 
-price2 = { 'p1' : prices[:-1].values.tolist() , 'p2' : prices[1:].values.tolist() }
+# price2 = { 'p1' : prices[:-1].values.tolist() , 'p2' : prices[1:].values.tolist() }
 
-df = pandas.DataFrame(data = price2)
+# df = pandas.DataFrame(data = price2)
 
-print(df)
+# cal = df.p1 / df.p2
 
+# df = pandas.DataFrame({'p1':prices[:-1].values , 'p2' : prices[1:].values })
+
+# df['sum'] = df['p1'] + df['p2']
+
+# print(df)
+
+
+# frame = pandas.DataFrame({'a': [1, 2, 3], 'b': [3, 5, 4]})
+
+# frame['c'] = frame['a'] + frame['b']
+
+# print(frame)
