59ec6f288b25f15e09ee277e8a068602de2d07d6
coolba73 <coolba73@outlook.com>
2017-11-14 20:20:59 +0900

    1

---
+++ CalBeta/CalBeta03.py
@@ -0,0 +1,31 @@
+import json
+import pandas as pd
+import numpy as np
+from collections import OrderedDict
+
+data = open("D:\FinInsightServices\PythonLec01\CalBeta\data\CalBeta02.dat",'r').read()
+
+source = json.loads(data)
+
+for k in source.keys():
+    source2 = json.loads(source[k])
+
+source = source2
+
+df_kospi = pd.DataFrame(source["KOSPI200"])
+
+var_kospi = np.var(df_kospi["LogYield"])
+
+re = []
+
+for k in source.keys():
+    if (k != 'KOSPI200'):
+        df = pd.DataFrame(source[k])
+        cov = np.cov(df_kospi["LogYield"] , df["LogYield"] )[0][1] 
+        beta = cov / var_kospi
+        item = {}
+        item["ItemCode"] = k
+        item["Beta"] = beta
+        re.append(item)
+
+print(json.dumps(re))
