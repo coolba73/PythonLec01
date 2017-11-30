a23e0d745640c7199b4b7480325d10039100f34e
coolba73 <coolba73@outlook.com>
2017-11-30 15:19:48 +0900

    1

--- CalCutRate2/CalCutRate2_01.py
+++ CalCutRate2/CalCutRate2_01.py
@@ -20,11 +20,9 @@ df = df.loc[df["Rank_x"] >= df["Rank_y"]]
 df = df.groupby(["ItemCode_x","Rank_x"], as_index=False).agg({"ER_y":np.sum})
 df.rename(columns = lambda x: x.replace('_x','').replace('_y',''), inplace = True)
 
-
 # 상관계수(ρ)를 0.5라고 하고 절사율을 계산한다.
 
 rho = 0.5
-
 df["cutrate"] = rho / (1-rho+(df["Rank"]*rho)) * df["ER"]
 
 print(df)
