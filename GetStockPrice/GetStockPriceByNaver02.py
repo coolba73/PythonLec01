79e752f7ac2d44049c4a97ca27a4af5af54ba300
coolba73 <coolba73@outlook.com>
2017-11-30 19:57:00 +0900

    1

--- GetStockPrice/GetStockPriceByNaver02.py
+++ GetStockPrice/GetStockPriceByNaver02.py
@@ -5,53 +5,93 @@ import datetime
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
   
-stockItem = '005930'
- 
-url = 'http://finance.naver.com/item/sise_day.nhn?code='+ stockItem
-html = urlopen(url) 
-source = BeautifulSoup(html.read(), "html.parser")
- 
-maxPage=source.find_all("table",align="center")
-mp = maxPage[0].find_all("td",class_="pgRR")
-mpNum = int(mp[0].a.get('href')[-3:])
+# stockItem = '005930'
 
-today = '{0:%Y-%m-%d}'.format(datetime.datetime.now())
-fromDate = '{0:%Y-%m-%d}'.format(datetime.datetime( int(today.split('-')[0]) - 1 ,int(today.split('-')[1]) ,int(today.split('-')[2]) ))
+# ========================================================================================================================================================
+def GetStockPrice(ItemCode):
+    
+    if (ItemCode == "KOSPI200"):
+        urlsource = 'http://finance.naver.com/sise/sise_index_day.nhn?code='
+        ItemCode = 'KPI200'
+    else:
+        urlsource = 'http://finance.naver.com/item/sise_day.nhn?code='
 
-yesStop = False;
-
-for page in range(1, mpNum+1):
-
-    url = 'http://finance.naver.com/item/sise_day.nhn?code=' + stockItem +'&page='+ str(page)
-    html = urlopen(url)
+    url = urlsource+ ItemCode
+    html = urlopen(url) 
     source = BeautifulSoup(html.read(), "html.parser")
-    srlists=source.find_all("tr")
-    isCheckNone = None
 
-    # if((page % 1) == 0):
-    #   time.sleep(1.50)
+    mpNum = 10000
 
-    for i in range(1,len(srlists)-1):
-        
-        if(srlists[i].span != isCheckNone):
+    today = '{0:%Y-%m-%d}'.format(datetime.datetime.now())
+    fromDate = '{0:%Y-%m-%d}'.format(datetime.datetime( int(today.split('-')[0]) - 1 ,int(today.split('-')[1]) ,int(today.split('-')[2]) ))
 
-            date = srlists[i].find_all("td",align="center")[0].text
-            closestr = srlists[i].find_all("td",class_="num")[0].text.strip()
+    yesStop = False;
 
-            date = date.replace(".","-")
-            closePrice = float(closestr.replace(',',''))
+    StockPriceList = []
+    dayInfo = {}
 
-            if (date < fromDate):
-                break
-                yesStop = True
+    preHtml = ''
 
-            print ( date, closePrice)
+    for page in range(1, mpNum+1):
 
-    if (yesStop ==  True):
+        url = urlsource + ItemCode +'&page='+ str(page)
+        html = urlopen(url)
+
+        if (preHtml == html):
             break
 
-print("end")
+        source = BeautifulSoup(html.read(), "html.parser")
+        srlists=source.find_all("tr")
+        isCheckNone = None
 
+        # if((page % 1) == 0):
+        #   time.sleep(1.50)
+
+        for i in range(1,len(srlists)-1):
+            
+            if(srlists[i].span != isCheckNone):
+               
+
+                # print ( srlists[i].find_all("td",class_="date")[0].text )
+                # print ( srlists[i].find_all("td",class_="number_1")[0].text )
+                # print(srlists)
+
+                if (ItemCode == 'KPI200'):
+                    date = srlists[i].find_all("td",class_="date")[0].text 
+                    closestr = srlists[i].find_all("td",class_="number_1")[0].text 
+                else:
+                    date = srlists[i].find_all("td",align="center")[0].text
+                    closestr = srlists[i].find_all("td",class_="num")[0].text.strip()                        
+
+                date = date.replace(".","-")
+                closePrice = float(closestr.replace(',',''))
+
+                if (date < fromDate):
+                    yesStop = True
+                    break
+
+                dayInfo = {}
+                dayInfo["Index"] = date
+                dayInfo["Close"] = closePrice
+
+                StockPriceList.append(dayInfo)
+
+                # print ( date, closePrice)
+
+        if (yesStop ==  True):
+                break
+        
+        preHtml = html
+
+    print("end")
+    result = {}
+    result[ItemCode] = StockPriceList
+    return result 
+# ========================================================================================================================================================
    
+# re = GetStockPrice("005930")
+re = GetStockPrice("KOSPI200")
+
+print(re)
 
 
