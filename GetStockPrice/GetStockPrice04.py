61b49f8737674b8694c0695e240a5dddf3618d5f
coolba73 <coolba73@outlook.com>
2017-12-04 08:42:42 +0900

    1

---
+++ GetStockPrice/GetStockPrice04.py
@@ -0,0 +1,34 @@
+import datetime
+import requests
+from io import StringIO
+from pandas.io.common import urlencode
+import pandas as pd
+
+BASE = 'http://finance.google.com/finance/historical'
+
+
+def get_params(symbol, start, end):
+    params = {
+        'q': symbol,
+        'startdate': start.strftime('%Y/%m/%d'),
+        'enddate': end.strftime('%Y/%m/%d'),
+        'output': "csv"
+    }
+    return params
+
+
+def build_url(symbol, start, end):
+    params = get_params(symbol, start, end)
+    return BASE + '?' + urlencode(params)
+
+
+start = datetime.datetime(2010, 1, 1)
+end = datetime.datetime.today()
+# sym = 'SPY'
+sym = 'KRX:035420'
+url = build_url(sym, start, end)
+
+data = requests.get(url).text
+data = pd.read_csv(StringIO(data), index_col='Date', parse_dates=True)
+
+print(data.head())
---
+++ GetStockPrice/GetStockPriceByNaver03.py
@@ -0,0 +1,106 @@
+import urllib
+import time
+import datetime
+import json
+ 
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+
+source = json.loads(open('D:\FinInsightServices\PythonLec01\GetStockPrice\data.dat','r').read())
+
+# print(source)
+
+# ========================================================================================================================================================
+def GetStockPrice(ItemCode):
+    
+    if (ItemCode == "KOSPI200"):
+        urlsource = 'http://finance.naver.com/sise/sise_index_day.nhn?code='
+        ItemCode = 'KPI200'
+    else:
+        urlsource = 'http://finance.naver.com/item/sise_day.nhn?code='
+
+    url = urlsource+ ItemCode
+    html = urlopen(url) 
+    source = BeautifulSoup(html.read(), "html.parser")
+
+    mpNum = 10000
+
+    today = '{0:%Y-%m-%d}'.format(datetime.datetime.now())
+    fromDate = '{0:%Y-%m-%d}'.format(datetime.datetime( int(today.split('-')[0]) - 1 ,int(today.split('-')[1]) ,int(today.split('-')[2]) ))
+
+    yesStop = False;
+
+    StockPriceList = []
+    dayInfo = {}
+
+    preHtml = ''
+
+    for page in range(1, mpNum+1):
+
+        url = urlsource + ItemCode +'&page='+ str(page)
+        html = urlopen(url)
+
+        if (preHtml == html):
+            break
+
+        source = BeautifulSoup(html.read(), "html.parser")
+        srlists=source.find_all("tr")
+        isCheckNone = None
+
+        # if((page % 1) == 0):
+        #   time.sleep(1.50)
+
+        for i in range(1,len(srlists)-1):
+            
+            if(srlists[i].span != isCheckNone):
+               
+
+                # print ( srlists[i].find_all("td",class_="date")[0].text )
+                # print ( srlists[i].find_all("td",class_="number_1")[0].text )
+                # print(srlists)
+
+                if (ItemCode == 'KPI200'):
+                    date = srlists[i].find_all("td",class_="date")[0].text 
+                    closestr = srlists[i].find_all("td",class_="number_1")[0].text 
+                else:
+                    date = srlists[i].find_all("td",align="center")[0].text
+                    closestr = srlists[i].find_all("td",class_="num")[0].text.strip()                        
+
+                date = date.replace(".","-")
+                closePrice = float(closestr.replace(',',''))
+
+                if (date < fromDate):
+                    yesStop = True
+                    break
+
+                dayInfo = {}
+                dayInfo["Index"] = date
+                dayInfo["Close"] = closePrice
+
+                StockPriceList.append(dayInfo)
+
+                # print ( date, closePrice)
+
+        if (yesStop ==  True):
+                break
+        
+        preHtml = html
+
+    # print("end")
+    result = {}
+    result[ItemCode] = StockPriceList
+    return result 
+# ========================================================================================================================================================
+   
+# re = GetStockPrice("005930")
+# re = GetStockPrice("KOSPI200")
+
+re = []
+
+for icode in source:
+    re.append(GetStockPrice(icode))
+
+print( json.dumps(re))
+
+
