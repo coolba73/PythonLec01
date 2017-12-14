ea5ca6e819b63dcbd3bc30d333a6c21fcae3c560
coolba73 <coolba73@outlook.com>
2017-12-14 20:22:45 +0900

    1

---
+++ Util/GetStockPrice.py
@@ -0,0 +1,45 @@
+
+import datetime
+import requests
+from io import StringIO
+from pandas.io.common import urlencode
+import pandas as pd
+import json
+
+# ==========================================================================================================================================================================
+BASE = 'http://finance.google.com/finance/historical'
+source = json.loads( open('D:\FinInsightServices\PythonLec01\GetStockPrice\data02.dat','r').read())
+
+# ==========================================================================================================================================================================
+def get_params(symbol, start, end):
+    params = {
+        'q': symbol,
+        'startdate': start.strftime('%Y/%m/%d'),
+        'enddate': end.strftime('%Y/%m/%d'),
+        'output': "csv"
+    }
+    return params
+
+# ==========================================================================================================================================================================
+def build_url(symbol, start, end):
+    params = get_params(symbol, start, end)
+    return BASE + '?' + urlencode(params)
+# ==========================================================================================================================================================================
+
+
+def Year(itemCode, year=1):
+    today = '{0:%Y-%m-%d}'.format(datetime.datetime.now())
+    start = datetime.datetime( int(today.split('-')[0]) - year ,int(today.split('-')[1]) ,int(today.split('-')[2]) )
+    end = datetime.datetime.now()
+    sym = "KRX:" + itemCode
+    url = build_url(sym, start, end)
+    data = requests.get(url).text
+    data = pd.read_csv(StringIO(data), index_col='Date', parse_dates=True)
+    return data
+
+
+
+    
+
+
+
---
+++ test.py
@@ -0,0 +1,9 @@
+import sys
+sys.path.append('D:/FinInsightServices/PythonLec01/Util')
+import GetStockPrice as sp
+
+re = sp.Year('KOSPI200')
+
+print(re)
+
+
